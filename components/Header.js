b7aeddd8ddbc5bf2283113d157602008e494d437
Nick <n.drobyshevski@pm.me>
2022-10-03 07:06:36 +0200

    feat: User login and register UI

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+
 import Link from "next/link";
 import styles from "@/styles/Header.module.css";
 import Search from "./Search";
@@ -21,6 +23,14 @@ export default function Header() {
           <li>
             <Link href="/events/add">
               <a>Add event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn-secondary btn-icon">
+                <FaSignInAlt />
+                Login
+              </a>
             </Link>
           </li>
         </ul>
---
+++ pages/account/login.js
@@ -0,0 +1,58 @@
+import { FaUser } from "react-icons/fa";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { useState, useEffect, useContext } from "react";
+import Link from "next/link";
+
+import Layout from "@/components/Layout";
+import styles from "@/styles/AuthFrom.module.css";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <Layout title="User Login">
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Log in
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <input type="submit" className="btn" value="Login" />
+        </form>
+        <p>
+          Dont have an account ? <Link href="/account/register">Register</Link>
+        </p>
+      </div>
+    </Layout>
+  );
+}
